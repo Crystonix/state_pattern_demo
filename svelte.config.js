6a6b92b370f6c260524adfd2b0a461c082fee815
Joshua Jung || jj050 <128308156+Crystonix@users.noreply.github.com>
2025-10-21 14:08:51 +0200

    Set NODE_ENV for build and update Svelte base path
    Adds NODE_ENV=production to the build step in the deploy workflow to ensure correct environment settings. Updates svelte.config.js to set the base path to '/state_pattern_demo' in production, improving deployment configuration.

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-		}
-	}
+    // Consult https://svelte.dev/docs/kit/integrations
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter({
+            fallback: '404.html'
+        }),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/state_pattern_demo' : ''
+        }
+    }
 };
 
 export default config;
